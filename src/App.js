4be18fa68b128aa3f6aea8ea3e8fec3960f104f8
Joseph Guillory <jagmanx99@yahoo.com>
2023-09-08 20:42:51 -0700

    header component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Learn React
-        </p>
-      </header>
+        <Header />
     </div>
   );
 }
